0f098965300657a9670ea287de085ae3ae4e91b0
Vladimir <motiw@icloud.com>
2023-02-27 21:19:02 +0600

    Initial Commit

---
+++ CookBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CookBook
+//
+//  Created by Vladimir on 27.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
