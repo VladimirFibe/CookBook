2da0751292325f926baa67bcd813dc9cb316abc6
Amsterdance26 <amsterdance26@gmail.com>
2023-02-28 20:19:15 +0700

    Добавлен titleLabel.text = "Let's cook with me"
    titleLabel.text = "Let's cook with me"

--- CookBook/StartScreenViewController.swift
+++ CookBook/StartScreenViewController.swift
@@ -11,6 +11,7 @@ class StartScreenViewController: UIViewController {
     
     let backgroundImage = UIImageView()
     let getStartedButton = UIButton()
+    let titleLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +19,12 @@ class StartScreenViewController: UIViewController {
         backgroundImage.contentMode = .scaleAspectFill
         backgroundImage.frame = view.bounds
         view.addSubview(backgroundImage)
+
+        titleLabel.text = "Let's cook with me"
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 36)
+        titleLabel.textColor = .white
         
+        view.addSubview(titleLabel)
 
         getStartedButton.setTitle("Get Started", for: .normal)
         getStartedButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
@@ -33,6 +39,10 @@ class StartScreenViewController: UIViewController {
         backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.bottomAnchor.constraint(equalTo: getStartedButton.topAnchor, constant: -30).isActive = true
         
         getStartedButton.translatesAutoresizingMaskIntoConstraints = false
         getStartedButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
