4d1368ea755adc26c7cd38d7cecaa4647ae7e4f3
Amsterdance26 <amsterdance26@gmail.com>
2023-02-28 21:54:06 +0700

    Transition from StartScreenViewController to viewController

--- CookBook/StartScreenViewController.swift
+++ CookBook/StartScreenViewController.swift
@@ -8,16 +8,17 @@
 import UIKit
 
 class StartScreenViewController: UIViewController {
-    
+
     let backgroundImage = UIImageView()
     let getStartedButton = UIButton()
     let titleLabel = UILabel()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         backgroundImage.image = UIImage(named: "backgroundImage")
         backgroundImage.contentMode = .scaleAspectFill
-        backgroundImage.frame = view.bounds
+        backgroundImage.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(backgroundImage)
 
         titleLabel.text = "let's cookWithMe"
@@ -27,38 +28,41 @@ class StartScreenViewController: UIViewController {
         let range = (attributedString.string as NSString).range(of: "'s")
         attributedString.addAttribute(.foregroundColor, value: UIColor.white, range: range)
         titleLabel.attributedText = attributedString
-
-        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(titleLabel)
 
         getStartedButton.setTitle("Get Started", for: .normal)
         getStartedButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
         getStartedButton.backgroundColor = .red
         getStartedButton.layer.cornerRadius = 10
-        //        getStartedButton.addTarget(self, action: #selector(getStartedButtonTapped), for: .touchUpInside)
-        view.addSubview(getStartedButton)
-        
-
-        backgroundImage.translatesAutoresizingMaskIntoConstraints = false
-        backgroundImage.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        titleLabel.bottomAnchor.constraint(equalTo: getStartedButton.topAnchor, constant: -30).isActive = true
-        
+        getStartedButton.addTarget(self, action: #selector(getStartedButtonTapped), for: .touchUpInside)
         getStartedButton.translatesAutoresizingMaskIntoConstraints = false
-        getStartedButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        getStartedButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50).isActive = true
-        getStartedButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        getStartedButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        view.addSubview(getStartedButton)
+
+        NSLayoutConstraint.activate([
+            backgroundImage.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: getStartedButton.topAnchor, constant: -30),
+
+            getStartedButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            getStartedButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            getStartedButton.widthAnchor.constraint(equalToConstant: 200),
+            getStartedButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+    @objc func getStartedButtonTapped() {
+        let viewController = ViewController()
+        viewController.modalPresentationStyle = .fullScreen
+        present(viewController, animated: true, completion: nil)
     }
 }
-
-//    @objc func getStartedButtonTapped() {
-//        let nextViewController = NextViewController()
-//        self.navigationController?.pushViewController(nextViewController, animated: true)
-//    }
-//}
