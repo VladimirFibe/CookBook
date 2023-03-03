a48ebf55dda01b1c44ac116bdefab0492f985228
Vladimir <motiw@icloud.com>
2023-03-03 14:14:48 +0600

    save

--- CookBook/Controllers/MainViewController.swift
+++ CookBook/Controllers/MainViewController.swift
@@ -1,18 +1,58 @@
 import UIKit
 
+enum MainItem: Hashable {
+    case trending(Int)
+    case popular(String)
+    case recent
+    case chef(Double)
+}
+
+struct MainRow: Hashable {
+    var index: Int
+    var title: String
+    var items: [MainItem]
+}
+
+enum MainSection: Int, CaseIterable {
+    case trending = 0
+    case popular
+    case recent
+    case chef
+}
+
+final class MainViewModel {
+    
+}
+private typealias DataSource = UICollectionViewDiffableDataSource<MainRow, MainItem>
+private typealias Snapshot = NSDiffableDataSourceSnapshot<MainRow, MainItem>
 class MainViewController: UIViewController {
 
-    var recipeView = SquareRecipe()
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: .rowLayout)
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.register(RecentRecipeCell.self, forCellWithReuseIdentifier: RecentRecipeCell.id)
+        return collectionView
+    }()
+    
+    private var dataSource: DataSource!
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .success10
-        view.addSubview(recipeView)
-        recipeView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            recipeView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            recipeView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            recipeView.widthAnchor.constraint(equalToConstant: 124),
-            recipeView.heightAnchor.constraint(equalToConstant: 190)
+            collectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            collectionView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            collectionView.widthAnchor.constraint(equalToConstant: 124),
+            collectionView.heightAnchor.constraint(equalToConstant: 190)
         ])
+        
+        configureDataSouce()
+    }
+    
+    private func configureDataSouce() {
+//        dataSource = DataSource(collectionView: collectionView) { collectionView, indexPath, itemIdentifier in
+//            
+//        }
     }
 }
