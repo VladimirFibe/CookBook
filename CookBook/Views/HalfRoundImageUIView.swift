b4658a916c74fa09543e56c410194345c421fc91
Amsterdance26 <amsterdance26@gmail.com>
2023-03-05 17:22:05 +0700

    Delete stack

--- CookBook/Views/HalfRoundImageUIView.swift
+++ CookBook/Views/HalfRoundImageUIView.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class HalfRoundImageUIView: UICollectionViewCell {
-
+    
     static let id = "HalfRoundImageUIView"
     
     let backgroundHalImageView: UIImageView = {
@@ -9,19 +9,19 @@ class HalfRoundImageUIView: UICollectionViewCell {
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
-
+    
     let circleImageView: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "Circle"))
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
-
+    
     let unionImageView: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "Union2"))
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
-
+    
     let imageView: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "Food"))
         imageView.contentMode = .scaleAspectFill
@@ -29,7 +29,7 @@ class HalfRoundImageUIView: UICollectionViewCell {
         imageView.layer.masksToBounds = true
         return imageView
     }()
-
+    
     let label: UILabel = {
         let label = UILabel()
         label.text = "Ifly's Kitchen"
@@ -38,7 +38,7 @@ class HalfRoundImageUIView: UICollectionViewCell {
         label.textColor = .black
         return label
     }()
-
+    
     let timeLabel: UILabel = {
         let label = UILabel()
         label.text = "Time"
@@ -47,8 +47,7 @@ class HalfRoundImageUIView: UICollectionViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-
-
+    
     let minutesLabel: UILabel = {
         let label = UILabel()
         label.text = "5 min"
@@ -57,48 +56,53 @@ class HalfRoundImageUIView: UICollectionViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-
-    lazy var stack: UIStackView = {
-        let stack = UIStackView(arrangedSubviews: [backgroundHalImageView, imageView, label])
-        stack.axis = .vertical
-        stack.alignment = .center
-        stack.spacing = 8
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        return stack
-    }()
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
     }
-
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setupView()
     }
-
+    
     private func setupView() {
-        contentView.addSubview(stack)
+        contentView.addSubview(backgroundHalImageView)
         backgroundHalImageView.addSubview(circleImageView)
         circleImageView.addSubview(unionImageView)
         backgroundHalImageView.addSubview(minutesLabel)
         backgroundHalImageView.addSubview(timeLabel)
-
+        backgroundHalImageView.addSubview(imageView)
+        backgroundHalImageView.addSubview(label)
+        
         NSLayoutConstraint.activate([
-            backgroundHalImageView.topAnchor.constraint(equalTo: imageView.centerYAnchor),
-            backgroundHalImageView.centerXAnchor.constraint(equalTo: imageView.centerXAnchor),
-            backgroundHalImageView.widthAnchor.constraint(equalToConstant: 150),
-            backgroundHalImageView.heightAnchor.constraint(equalToConstant: 176),
+            
             imageView.widthAnchor.constraint(equalToConstant: 110),
             imageView.heightAnchor.constraint(equalToConstant: 110),
+
+            backgroundHalImageView.topAnchor.constraint(equalTo: imageView.centerYAnchor),
+            backgroundHalImageView.bottomAnchor.constraint(equalTo: minutesLabel.bottomAnchor, constant: 11),
+
+
             circleImageView.trailingAnchor.constraint(equalTo: backgroundHalImageView.trailingAnchor, constant: -11),
             circleImageView.bottomAnchor.constraint(equalTo: backgroundHalImageView.bottomAnchor, constant: -11),
+            circleImageView.widthAnchor.constraint(equalToConstant: 24),
+            circleImageView.heightAnchor.constraint(equalToConstant: 24),
+            
             unionImageView.centerXAnchor.constraint(equalTo: circleImageView.centerXAnchor),
             unionImageView.centerYAnchor.constraint(equalTo: circleImageView.centerYAnchor),
             unionImageView.widthAnchor.constraint(equalToConstant: 10.96),
             unionImageView.heightAnchor.constraint(equalToConstant: 13.67),
+            
+            label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10),
+            label.leadingAnchor.constraint(equalTo: backgroundHalImageView.leadingAnchor, constant: 12),
+            label.trailingAnchor.constraint(equalTo: backgroundHalImageView.trailingAnchor, constant: -12),
+            label.heightAnchor.constraint(equalToConstant: 30),
+            
             timeLabel.leadingAnchor.constraint(equalTo: backgroundHalImageView.leadingAnchor, constant: 12),
             timeLabel.bottomAnchor.constraint(equalTo: minutesLabel.topAnchor, constant: -4),
+            
             minutesLabel.leadingAnchor.constraint(equalTo: backgroundHalImageView.leadingAnchor, constant: 12),
             minutesLabel.bottomAnchor.constraint(equalTo: backgroundHalImageView.bottomAnchor, constant: -11)
         ])
