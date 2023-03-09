3bd81b05f495185336939eb6ee49f851ca7207c7
Vladimir <motiw@icloud.com>
2023-03-10 00:31:13 +0600

    fix

--- CookBook/Network/RecipeHTTPClient.swift
+++ CookBook/Network/RecipeHTTPClient.swift
@@ -9,17 +9,24 @@ enum NetworkError: Error {
 final class RecipeHTTPClient {
     static let shared = RecipeHTTPClient()
     let apiKey = "a67a5241c34f45429f75c2d8a1858a67"
+    let baseURL = "https://api.spoonacular.com/"
     private init() {}
     
-    func getRandomRecipes(number: Int) async throws -> [Recipe] {
-        guard let url = URL(string: "https://api.spoonacular.com/recipes/random?number=\(number)") else { throw NetworkError.invalidURL }
-        var request = URLRequest(url: url, timeoutInterval: .infinity)
-        request.addValue(apiKey, forHTTPHeaderField: "x-api-key")
-        request.httpMethod = "GET"
-        let (data, response) = try await URLSession.shared.data(for: request)
+    func makeRequest<T: Decodable>(
+        path: String,
+        params: [String: String] = [:]
+    ) async throws -> T {
+        var queryItems = [URLQueryItem(name: "apiKey", value: apiKey)]
+        for (name, value) in params {
+            queryItems.append(URLQueryItem(name: name, value: value))
+        }
+        var components = URLComponents(string: baseURL+path)
+        components?.queryItems = queryItems
+        guard let url = components?.url else { throw NetworkError.invalidURL }
+        let (data, response) = try await URLSession.shared.data(from: url)
         guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw NetworkError.invalidResponse }
-        guard let recipeResponse = try? JSONDecoder().decode(RecipeResponse.self, from: data) else { throw NetworkError.decodingError }
-        return recipeResponse.recipes
+        guard let recipeResponse = try? JSONDecoder().decode(T.self, from: data) else { throw NetworkError.decodingError }
+        return recipeResponse
     }
     
     func getRecipes() -> [Recipe] {
