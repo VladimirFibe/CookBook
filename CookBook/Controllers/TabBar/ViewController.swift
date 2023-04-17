3582d0d7fcf8ec1086d59ef26e7e2a08945de98c
DmitryLorents <zmey.task@gmail.com>
2023-04-17 21:18:02 +0600

    - images for CreateRecipeVc added

--- CookBook/Controllers/TabBar/ViewController.swift
+++ CookBook/Controllers/TabBar/ViewController.swift
@@ -5,12 +5,20 @@ class ViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+
     }
     
     private func configureUI() {
         setValue(CustomTabBar().self, forKey: "tabBar")
-//        guard let tabBar = self.tabBar as? CustomTabBar else { return }
-
+        guard let tabBar = self.tabBar as? CustomTabBar else { return }
+        
+        tabBar.didTabButton = {
+            let createRecipe = UINavigationController(rootViewController: CreateRecipeViewController())
+            createRecipe.modalPresentationStyle = .fullScreen
+            self.present(createRecipe, animated: true)
+            
+        }
+        
         let home = UINavigationController(rootViewController: MainViewController())
         home.tabBarItem = UITabBarItem(title: nil,
                                        image: UIImage(named: "HomeInactive"),
@@ -35,7 +43,7 @@ class ViewController: UITabBarController {
                                           selectedImage: UIImage(named: "ProfileActive")?.withRenderingMode(.alwaysOriginal))
         profile.view.backgroundColor = .systemBackground
         
-        
+       
         
         setViewControllers([home, bookmark, notification, profile], animated: true)
     }
