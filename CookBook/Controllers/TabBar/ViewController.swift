1c5bdcadeff61653417f1006ce59182da0acd547
DmitryLorents <zmey.task@gmail.com>
2023-04-19 12:19:37 +0600

    - modify TabBars's icons' positions

--- CookBook/Controllers/TabBar/ViewController.swift
+++ CookBook/Controllers/TabBar/ViewController.swift
@@ -1,11 +1,10 @@
 import UIKit
 
 class ViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
-
     }
     
     private func configureUI() {
@@ -19,32 +18,35 @@ class ViewController: UITabBarController {
         
         tabBar.tintColor = .primary30
         
-        let home = MainViewController()
-        home.tabBarItem = UITabBarItem(title: nil,
-                                       image: UIImage(named: "HomeInactive"),
-                                       selectedImage: UIImage(named: "HomeActive")?.withRenderingMode(.alwaysOriginal))
+        let home = UINavigationController(rootViewController: MainViewController())
+        let homeInactiveImage = UIImage(named: "HomeInactive")
+        let homeActiveImage = UIImage(named: "HomeActive")?.withRenderingMode(.alwaysOriginal)
+        let homeItem = UITabBarItem(title: nil, image: homeInactiveImage, selectedImage: homeActiveImage)
+        home.tabBarItem = homeItem
+        homeItem.imageInsets = UIEdgeInsets(top: 10, left: 0, bottom: -10, right: 0)
+        homeItem.title = nil
         home.view.backgroundColor = .systemBackground
         
         let bookmark = SavedRecipesVC()
-        bookmark.tabBarItem = UITabBarItem(title: nil,
-                                           image: UIImage(named: "BookmarkInactive"),
-                                           selectedImage: UIImage(named: "BookmarkActive")?.withRenderingMode(.alwaysOriginal))
+        let bookmarkItem =  UITabBarItem(title: nil,image: UIImage(named: "BookmarkInactive"),selectedImage: UIImage(named: "BookmarkActive")?.withRenderingMode(.alwaysOriginal))
+        bookmarkItem.imageInsets = UIEdgeInsets(top: 11, left: -15, bottom: -11, right: 15)
+        bookmark.tabBarItem = bookmarkItem
         bookmark.view.backgroundColor = .systemBackground
         
-        let toBuyItem = UIViewController()
-        toBuyItem.tabBarItem = UITabBarItem(title: nil,
-                                            image: UIImage(systemName: "cart"), selectedImage: UIImage(systemName: "cart.fill"))
-        toBuyItem.view.backgroundColor = .systemBackground
+        let toBuy = UIViewController()
+        let toBuyItem = UITabBarItem(title: nil,image: UIImage(named: "NotificationInactive"), selectedImage: UIImage(named: "NotificationActive"))
+        toBuyItem.imageInsets = UIEdgeInsets(top: 10, left: 15, bottom: -10, right: -15)
+        toBuy.tabBarItem = toBuyItem
+        toBuy.view.backgroundColor = .systemBackground
         
         let profile = UIViewController()
-        profile.tabBarItem = UITabBarItem(title: "",
-                                          image: UIImage(named: "ProfileInactive"),
-                                          selectedImage: UIImage(named: "ProfileActive")?.withRenderingMode(.alwaysOriginal))
+        let profileItem = UITabBarItem(title: "", image: UIImage(named: "ProfileInactive"),selectedImage: UIImage(named: "ProfileActive")?.withRenderingMode(.alwaysOriginal))
+        profileItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -5, right: 0)
+        profile.tabBarItem = profileItem
         profile.view.backgroundColor = .systemBackground
         
-       
+        setViewControllers([home, bookmark, toBuy, profile], animated: true)
         
-        setViewControllers([home, bookmark, toBuyItem, profile], animated: true)
     }
 }
 
