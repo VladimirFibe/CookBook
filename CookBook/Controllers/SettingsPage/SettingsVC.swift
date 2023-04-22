03171fe6a8dab3a94e3c3d6fe8730110f8746cb0
DmitryLorents <zmey.task@gmail.com>
2023-04-22 16:10:59 +0600

    - start to create UI

--- CookBook/Controllers/SettingsPage/SettingsVC.swift
+++ CookBook/Controllers/SettingsPage/SettingsVC.swift
@@ -7,9 +7,28 @@
 
 import UIKit
 
+struct SettingRow {
+    let label: String
+    let value: String
+}
+
 class SettingsVC: UIViewController {
 
     var appImageView: UIImageView!
+    var tableViewSettings: UITableView!
+    let headersArray = ["Application", "Developers"]
+    let applicationArray: [SettingRow] =
+    [SettingRow(label: "Product", value: "Cook Book"),
+     SettingRow(label: "Compatability", value: "Iphone"),
+     SettingRow(label: "Design", value: "Somebody arabian"),
+     SettingRow(label: "Website", value: "https://spoonacular.comhttps://spoonacular.com"),
+     SettingRow(label: "Varsion", value: "1.0")]
+    let developersArray: [SettingRow] = [
+    SettingRow(label: "", value: "Vladimir Fibe"),
+    SettingRow(label: "", value: "Dmitry Lorents"),
+    SettingRow(label: "", value: "Abylay Akhmetov")]
+    let settingsRowsArray: [ [SettingRow] ] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +41,7 @@ class SettingsVC: UIViewController {
     func setLayout() {
         
         view.backgroundColor = .systemBackground
+        let settingsRowsArray = [applicationArray, developersArray]
         
         appImageView = UIImageView()
         appImageView.image = UIImage(named: "AppIcon")
@@ -29,6 +49,11 @@ class SettingsVC: UIViewController {
         appImageView.translatesAutoresizingMaskIntoConstraints = false
         appImageView.layer.cornerRadius = 10
         view.addSubview(appImageView)
+        
+        tableViewSettings = UITableView(frame: CGRect.zero, style: .grouped)
+        tableViewSettings.translatesAutoresizingMaskIntoConstraints = false
+        tableViewSettings.delegate = self
+        tableViewSettings.dataSource = self
     }
     
     func setConstraints() {
@@ -41,3 +66,22 @@ class SettingsVC: UIViewController {
     }
 }
 
+//MARK: - TableView Delegate
+
+extension SettingsVC: UITableViewDelegate {
+    
+}
+
+//MARK: - TableView DataSource
+
+extension SettingsVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
