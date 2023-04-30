778a616eec02ab4ad58547c1c217f3024b7e241d
Amsterdance26 <amsterdance26@gmail.com>
2023-05-01 03:53:28 +0700

    change screen

--- CookBook/App/SceneDelegate.swift
+++ CookBook/App/SceneDelegate.swift
@@ -4,7 +4,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     var startScreen = StartScreenViewController()
-    var tabBar = UINavigationController(rootViewController: ViewController() )
+    var tabBar = UINavigationController(rootViewController: RecipeDetailViewController() )
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
